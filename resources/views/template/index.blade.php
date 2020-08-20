df1816be89e7b1c7105e70cc901c9be7b3bcb76a
benittoraj <brendonbeni42@gmail.com>
2020-08-20 09:38:42 +0530

    preloder added

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -1,42 +1,71 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
-
-    {{-- <link rel="stylesheet" type="text/css" href="{{asset('css/templateStyle.css')}}"> --}}
-  <script src="{{asset('js/jquery.min.js')}}"></script>
-   
-    <link rel="stylesheet" href="{{asset('datatable/dataTables.min.css')}}">
-    <script src="{{asset('datatable/dataTables.min.js')}}" defer></script>
-
-    
-       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
-     <script src="{{asset('js/post.js')}}"></script>
-   
-    <script src="{{asset('plugins/toastr/js/toastr.min.js')}}"></script>
-    <link rel="stylesheet" type="text/css" href="{{asset('plugins/toastr/css/toastr.min.css')}}">
-  
-    <script src="{{asset('js/taginput.min.js')}}"></script>
-  <link rel="stylesheet" type="text/css" href="{{asset('css/taginput.min.css')}}">
-   <script src="{{asset('js/taginput.min.js')}}"></script>
-</head>
-<body>
-    @include('template.navcontent')
-    <div class="row no-gutters content"> 
-       @include('template.leftsidebar')
-        <div class="col-md-8 col-lg-8 col-xs-12 main-content">
-               <div class="search-results">
-                </div>
-              @yield('content')
+    <head>
+        <meta charset="utf-8">
+            <meta content="width=device-width, initial-scale=1.0" name="viewport">
+                <title>
+                    Blog
+                </title>
+                <link href="{{asset('css/welcome.css')}}" rel="stylesheet">
+                    <link href="{{asset('css/preloader.css')}}" rel="stylesheet">
+                        {{--
+                        <link href="{{asset('css/templateStyle.css')}}" rel="stylesheet" type="text/css">
+                            --}}
+                            <script src="{{asset('js/jquery.min.js')}}">
+                            </script>
+                            <link href="{{asset('datatable/dataTables.min.css')}}" rel="stylesheet">
+                                <script defer="" src="{{asset('datatable/dataTables.min.js')}}">
+                                </script>
+                                <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+                                    <script src="{{asset('js/post.js')}}">
+                                    </script>
+                                    <script src="{{asset('plugins/toastr/js/toastr.min.js')}}">
+                                    </script>
+                                    <link href="{{asset('plugins/toastr/css/toastr.min.css')}}" rel="stylesheet" type="text/css">
+                                        <script src="{{asset('js/taginput.min.js')}}">
+                                        </script>
+                                        <link href="{{asset('css/taginput.min.css')}}" rel="stylesheet" type="text/css">
+                                            <script src="{{asset('js/taginput.min.js')}}">
+                                            </script>
+                                        </link>
+                                    </link>
+                                </link>
+                            </link>
+                        </link>
+                    </link>
+                </link>
+            </meta>
+        </meta>
+    </head>
+    <body>
+        <div class="preloader-content">
+            <div class="loading">
+                <p>
+                    loading
+                </p>
+                <span>
+                </span>
+            </div>
         </div>
-        @include('template.rightsidebar')
-    </div>
-
-</body>
-<script src="{{asset('js/app.js')}}"></script>
+        <div class="section">
+            @include('template.navcontent')
+            <div class="row no-gutters content">
+                @include('template.leftsidebar')
+                <div class="col-md-8 col-lg-8 col-xs-12 main-content">
+                    <div class="search-results">
+                    </div>
+                    @yield('content')
+                </div>
+                @include('template.rightsidebar')
+            </div>
+        </div>
+    </body>
+    <script src="{{asset('js/app.js')}}">
+    </script>
+    <script type="text/javascript">
+        window.addEventListener('load',()=>{
+    const preload = document.querySelector('.preloader-content');
+        preload.classList.add('disappear');
+    })
+    </script>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
                 <title>
                     Blog-Mallow
                 </title>
+                <link href="{{asset('css/preloader.css')}}" rel="stylesheet">
                 <link href="{{asset('css/welcome.css')}}" rel="stylesheet">
                     <link crossorigin="anonymous" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" rel="stylesheet">
                         <link href="{{asset('datatable/dataTables.min.css')}}" rel="stylesheet">
@@ -18,8 +19,24 @@
                             <script src="{{asset('plugins/toastr/js/toastr.min.js')}}">
                             </script>
                             <link href="{{asset('plugins/toastr/css/toastr.min.css')}}" rel="stylesheet" type="text/css">
+                            </link>
+                        </link>
+                    </link>
+                </link>
+            </meta>
+        </meta>
     </head>
     <body>
+        <div class="preloader-content">
+            <div class="loading">
+                <p>
+                    loading
+                </p>
+                <span>
+                </span>
+            </div>
+        </div>
+        <div class="section">
         @include('template.navcontent')
         <div class="row no-gutters content">
             @include('template.leftsidebar')
@@ -30,17 +47,17 @@
                     <div class="posts">
                         @foreach($posts as $post)
                         <li>
-                        <a href="{{url('/home/'.$post->id)}}">
-                            <div class="post-title">
-                                {{$post->title}}
-                                <span>
-                                    {{$post->created_at}}
-                                </span>
-                            </div>
-                            <div class="post-description">
-                                {{substr($post->description,0,100)}}...
-                            </div>
-                         </a>
+                            <a href="{{url('/home/'.$post->id)}}">
+                                <div class="post-title">
+                                    {{$post->title}}
+                                    <span>
+                                        {{$post->created_at}}
+                                    </span>
+                                </div>
+                                <div class="post-description">
+                                    {{substr($post->description,0,100)}}...
+                                </div>
+                            </a>
                         </li>
                         @endforeach
                     </div>
@@ -51,6 +68,16 @@
             </div>
             @include('template.rightsidebar')
         </div>
+        </div>
     </body>
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/app.js')}}">
+    </script>
+    <script type="text/javascript">
+        window.addEventListener('load',()=>{
+    const preload = document.querySelector('.preloader-content');
+        preload.classList.add('disappear');
+
+
+})
+    </script>
 </html>
